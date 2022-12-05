ebc93abf364b4b18b641839ae159e6c546592afb
Argha Chakrabarty <ac380012@gmail.com>
2022-12-05 13:52:55 +0000

    set up isotp-can

---
+++ misc.py
@@ -0,0 +1,35 @@
+import sys
+
+
+def setup_config():
+    can.rc['interface'] = 'socketcan'
+    can.rc['channel'] = 'can0'
+    can.rc['bitrate'] = 500000
+
+
+def parse_args(args: list[str]):
+    _msg = sys.argv[1].strip().split("#")
+
+    iD = _msg[0]
+    data = _msg[1]
+
+    if 'x' in iD:
+        iD = int(iD, 16)
+    else:
+        iD = int(iD)
+
+    if iD > (2**29 - 1):
+        print("CAN ID exceeds 2^11 - 1")
+        sys.exit(1)
+
+    if 'b' in data:
+        data = bytes(data[2:-1], "utf-8")
+    else:
+        data = list(map(int, data.strip().split(",")))
+
+    if len(data) > 8:
+        print("Frame length can't be more than 8")
+
+    print(iD, data)
+
+    return iD, data
--- send.py
+++ send.py
@@ -1,31 +1,46 @@
-import can
 import sys
+import logging
+import time
+
+import isotp
+import can
 
 from ip_link import Ip_link
+from misc import setup_config, parse_args
 
 can.rc['interface'] = 'socketcan'
 can.rc['channel'] = 'can0'
 can.rc['bitrate'] = 500000
 
+def error_handler(error):
+    logging.warning("IsoTp error: %s - %s" % (error.__class__.__name__, str(error)))
+
 def send_msg(iD, data):
     with Ip_link() as ip_link:
         with can.Bus() as bus:
-            msg = can.Message(
-                arbitration_id=0x1111, data=[11, 12, 13, 14, 15, 16, 17, 18]
-            )
+            addr = isotp.Address(isotp.AddressingMode.Normal_29bits, rxid=0x123, txid=0x456,
+                    # source_address=0x55, target_address=0xAA
+                )
 
-            try:
-                bus.send(msg)
-                print(f"Message Sent on channel: {bus.channel_info}")
-            except can.CanError:
-                print("ERROR OCCURED", can.CanError)
+            stack = isotp.CanStack(bus, address=addr, error_handler=error_handler)
+            stack.send(b'helloworld!')
+
+            while stack.transmitting():
+                stack.process()
+                time.sleep(stack.sleep_time())
+
+            # msg = can.Message(
+            #     arbitration_id=iD,
+            #     data=data,
+            # )
+
+            # try:
+            #     bus.send(msg)
+            #     print(f"Message Sent on channel: {bus.channel_info}")
+            # except can.CanError:
+            #     print("ERROR OCCURED", can.CanError)
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
-        print("[ID#MSG]")
-        _msg = sys.argv[1].strip().split("#")
-
-        iD = _msg[0]
-        data = _msg[1]
-
+        iD, data = parse_args(sys.argv)
         send_msg(iD, data)
