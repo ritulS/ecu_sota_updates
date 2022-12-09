b52beb86b827e5683990ee1ad87bfe242a359226
Ritul <rituls619@gmail.com>
2022-12-09 15:49:49 +0530

    ecu manifest generation done

---
+++ primary_ecu/ecu_man.py
@@ -0,0 +1,55 @@
+
+# Generates ECU manifest
+# Input: symmetric key of ECU, image 
+
+# AES for symmetric signature
+# SHA-512/256 for hashing
+
+from dataclasses import dataclass
+from Crypto.Hash import SHA512
+from Crypto.Cipher import AES
+
+@dataclass
+class ecu_manifest:
+    cipher_text: bytes
+    tag: bytes
+    sr_num: str
+
+class ecu_input:
+    id: str
+    sym_key: str
+    img_byte_str: str
+
+    def __init__(self, file_name: str) -> None:
+        self.file_name =  file_name
+        pass
+
+    def _load_file(self, file_name):
+        #file = 'file'
+        #final output => byte array
+        pass
+
+    def _hash_and_sign(self):
+        img_byte_str = self._load_file("./file.bin")
+        key = b'ecuKey' #Use the hardcoded key here
+        img_hash = SHA512.new(img_byte_str)
+        cipher = AES.new(key, AES.MODE_EAX)
+
+        nonce = cipher.nonce
+        cipher_text, tag = cipher.encrypt_and_digest(img_hash)
+
+        return cipher_text, tag 
+
+
+    def gen_ecu_manifest(self) -> ecu_manifest:
+        cipher_text, tag = self._hash_and_sign(img_byte_str=self._load_file())
+        
+        return  ecu_manifest(
+            cipher_text= cipher_text,
+            sr_num = "harcoded",
+            tag = tag
+        )
+        # return manifest
+        
+        
+        
---
+++ primary_ecu/vvm.py
@@ -0,0 +1,24 @@
+# Script to generate VVM 
+# Will run on primary ECU
+# Inputs: Hash of all secondary ecus and their respective Sym keys
+# Output: Concatenation of Signed hashes of all secondary ecus
+
+
+# ed255196 for signature
+# SHA-512/256 for hashing
+
+
+class prim_ecu:
+    id: str
+    sym_key: str
+    img_hash: str
+
+
+    def req_ecu_man(self):
+        pass
+
+    def receive_ecu_man(self):
+        pass
+    
+    def gen_vvm(self):
+        pass
